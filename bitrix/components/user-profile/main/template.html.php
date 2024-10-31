3172ec972da833ab72598466c8b3b5daf173499d
unknown <denedoseikin@mail.ru>
2024-10-31 22:38:02 +0700

    fix: исправить замечания

---
+++ bitrix/components/user-profile/main/template.html.php
@@ -0,0 +1,13 @@
+
+<div class="user-profile"> 
+  <button class="user-profile__button" onclick="changeAvatar()" type="button"> <img class="user-profile__avatar" src="./img/user_avatar.png" alt="Аватар"/></button>
+  <div class="user-profile__info"> 
+    <h2 class="user-profile__name">Валентинов Георгий Викторович</h2><span class="user-profile_job">Должность     </span>
+  </div>
+</div>
+<div class="tabs tabs_projects">
+  <button class="tab-button tab-button-m" onclick="" type="button" value="Настройки">Настройки</button>
+  <button class="tab-button tab-button-m" onclick="" type="button" value="Мои подписки">Мои подписки</button>
+  <button class="tab-button tab-button-m" onclick="" type="button" value="Мои инициативные проекты">Мои инициативные проекты</button>
+  <button class="tab-button tab-button-m" onclick="" type="button" value="Мои обращения">Мои обращения</button>
+</div>
