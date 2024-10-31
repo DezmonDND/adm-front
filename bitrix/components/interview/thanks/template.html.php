466ce927c87f10796503f6224de7bd0c37c37716
unknown <denedoseikin@mail.ru>
2024-10-31 23:25:35 +0700

    feat: добавить пагинацию

--- bitrix/components/interview/thanks/template.html.php
+++
@@ -1,9 +0,0 @@
-
-<div class="thanks">
-  <div class="thanks__container"><img class="thanks__icon" src="../icons/check_icon.svg" alt="Иконка Успех"/>
-    <div class="thanks__description"> 
-      <h2 class="thanks__title">Благодарим вас за принятие участия в опросе!</h2>
-      <p class="thanks__text">Ваш вклад помогает городу становиться лучше. Примите участие и в других опросах - вместе мы сделаем город Сургут лучше!</p>
-    </div><a class="button button_link button_blue button_thanks" href="/" target="_blank"><span class="button_span">Перейти к опросам </span></a>
-  </div>
-</div>
