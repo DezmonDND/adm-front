6afc4003d12d791cf6b76e76db37da0de159bc34
Alex Bozhko <alex.bozhko@mebot24.ru>
2024-11-07 01:07:42 +0300

    update with button

--- bitrix/template/script.js
+++ bitrix/template/script.js
@@ -1,3 +1,27 @@
+function openSelect() {
+  const selectMenu = document.querySelector(".select_multiple-menu");
+  selectMenu.style.display =
+    selectMenu.style.display === "none" ? "flex" : "none";
+}
+
+function selectOption(element) {
+  element.classList.toggle("select_multiple-option_selected");
+}
+
+function getLength() {
+  const textarea = document.querySelector(".textarea");
+  const counterCurrent = document.querySelector(".current");
+  const counterMax = document.querySelector(".max");
+
+  const textLength = textarea.value.length;
+  counterCurrent.textContent = textLength;
+  if (textLength > Number(counterMax.textContent)) {
+    textarea.style.borderColor = "#D10404";
+  } else {
+    textarea.style.borderColor = "";
+  }
+}
+
 const msDropdownList = document.querySelector(".ms__dropdown");
 const msDropdownItems = document.querySelectorAll(".ms__dropdown-item");
 const multiSelect = document.querySelector('input[name="multiSelect"]');
@@ -71,28 +95,4 @@ function deleteElement(text) {
 function updateMultiSelectValue(e) {
   multiSelect.value = selectedValues.join(", ");
   getValues(e);
-}
-
-function getLength() {
-  const textarea = document.querySelector(".textarea");
-  const counterCurrent = document.querySelector(".current");
-  const counterMax = document.querySelector(".max");
-
-  const textLength = textarea.value.length;
-  counterCurrent.textContent = textLength;
-  if (textLength > Number(counterMax.textContent)) {
-    textarea.style.borderColor = "#D10404";
-  } else {
-    textarea.style.borderColor = "";
-  }
-}
-
-function openSelect() {
-  const selectMenu = document.querySelector(".select_multiple-menu");
-  selectMenu.style.display =
-    selectMenu.style.display === "none" ? "flex" : "none";
-}
-
-function selectOption(element) {
-  element.classList.toggle("select_multiple-option_selected");
 }
