4e4f44efe0423020f96fe7c3c21f66b9028eefbc
unknown <denedoseikin@mail.ru>
2024-11-12 00:04:39 +0700

    fix: исправить замечания, удалить лишнее из кода

---
+++ bitrix/components/biography/main/biography/template.html.php
@@ -0,0 +1,54 @@
+
+<div class="biography"> 
+  <div class="biography__container">
+    <div class="biography__contacts-content">        <img class="biography__avatar" src="./resources/kirilenko_avatar.png"/>
+      <div class="biography__contacts"> 
+        <div class="biography__phones">
+          <h4 class="biography__phones-title">Секретарь приемной</h4><span class="biography__phone">тел. +7 (3462) 52-20-01</span><a class="biography__phone biography__link" href="mailto:priemnaya@admsurgut.ru">e-mail: priemnaya@admsurgut.ru  </a>
+        </div>
+        <div class="biography__phones">
+          <h4 class="biography__phones-title">Отдел делопроизводства</h4><span class="biography__phone">тел. +7 (3462) 52 21 57</span><span class="biography__phone">тел. +7 (3462) 52 21 75</span><span class="biography__phone">факс +7 (3462) 52-21-82</span><a class="biography__phone biography__link" href="mailto:gorod@admsurgut.ru">e-mail: gorod@admsurgut.ru</a>
+        </div>
+        <div class="biography__phones">
+          <h4 class="biography__phones-title">Страницы в социальных сетях</h4><a class="biography__phone biography__link" href="https://vk.com/filatov_surgut">https://vk.com/filatov_surgut</a><a class="biography__phone biography__link" href="https://t.me/filatov_surgut ">https://t.me/filatov_surgut   </a>
+        </div>
+      </div>
+    </div>
+    <div class="biography__info">
+      <div class="biography__info-content">
+        <div class="biography__user">                       
+          <div class="biography__user-info">
+            <h2 class="biography__name">Кириленко Артем Михайлович</h2><span class="biography__job">Заместитель главы города Сургута</span>
+          </div>
+        </div>
+        <div class="biography__description"> 
+          <p>Родился 1 июня 1973 года в Курске, в 1988 году после окончания 8-го класса вместе с родителями переехал в Югру. Здесь получил первое образование в <span class="biography__description-span">Нижневартовском нефтяном техникуме</span> по специальности «Бурение нефтяных и газовых скважин». </p>
+          <p>В 2004 году закончил Уральскую академию государственной службы по специальности «Государственное и муниципальное управление». Имеет квалификационную степень в сфере управления <span class="biography__description-span">MBA (Master of Business Administration)</span> Российской академии народного хозяйства и государственной службы при Президенте Российской Федерации. </p>
+          <p class="biography__description-span"> Кандидат социологических наук. </p>
+          <p>До 2012 года занимался предпринимательской деятельностью. </p>
+          <p>Начиная с 2006 года дважды избирался депутатом муниципального уровня и дважды – депутатом Думы Югры. </p>
+          <p>С 2013 по 2015 год работал заместителем Губернатора ХМАО – Югры. </p>
+          <p>Курировал внутриполитический блок. </p>
+          <p>С 2016 по 2021 год возглавлял Комитет по бюджету, финансам и налоговой политике Думы Югры. </p>
+          <p>27 февраля 2021 года избран Главой города Сургута. </p>
+          <p class="biography__description-span">Член партии «ЕДИНАЯ РОССИЯ».</p>
+          <p>Женат, воспитывает двоих сыновей. </p>
+          <div class="biography__more-contacts">
+            <div class="biography__more-contacts-content">
+              <h5 class="biography__more-contacts-title">Помощник Главы города</h5>
+              <p>Журавская Татьяна Владимировна, 8 (3462) 522-400</p>
+            </div>
+            <div class="biography__more-contacts-content">
+              <h5 class="biography__more-contacts-title">Советник Главы города</h5>
+              <p>Ванькова Мария Николаевна, 8 (3462) 522-275</p>
+            </div>
+            <div class="biography__more-contacts-content">
+              <h5 class="biography__more-contacts-title">Советник Главы города</h5>
+              <p>Фомагин Валерий Борисович, 8 (3462) 522-404        </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
