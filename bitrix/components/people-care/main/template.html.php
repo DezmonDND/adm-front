5d75fbcf3b275bfefc01246db9a8e7a008f7aa46
unknown <denedoseikin@mail.ru>
2024-11-11 16:37:55 +0700

    feat: сделать сборки для битрикс

---
+++ bitrix/components/people-care/main/template.html.php
@@ -0,0 +1,38 @@
+
+<div class="main-people-care"> 
+  <div class="main-title">
+    <div class="main-title__container">
+      <h1 class="main-title__title">С заботой о жителях</h1>
+      <div class="slider"> <a class="slider__link-prev">
+          <ion-icon name="arrow-back-outline"></ion-icon></a><a class="slider__link-next">
+          <ion-icon name="arrow-forward-outline"></ion-icon></a></div>
+    </div>
+  </div>
+  <div class="main-people-care__container">
+    <div class="main-people-care__card-list"> 
+      <div class="people-care-card">
+        <div class="people-care-card__container">
+          <h2 class="people-care-card__title">Лицам с ограниченными возможностями здоровья</h2><a class="button button_link button_blue button_size_m  button_icon-right" href="/" attributes="{}"><span class="button_span">Подробнее</span>
+            <ion-icon class="icon" name="arrow-forward"></ion-icon></a>
+        </div>
+      </div>
+      <div class="people-care-card">
+        <div class="people-care-card__container">
+          <h2 class="people-care-card__title">Бюджет для граждан Сургута</h2><a class="button button_link button_blue button_size_m  button_icon-right" href="/" attributes="{}"><span class="button_span">Подробнее</span>
+            <ion-icon class="icon" name="arrow-forward"></ion-icon></a>
+        </div>
+      </div>
+      <div class="people-care-card">
+        <div class="people-care-card__container">
+          <h2 class="people-care-card__title">Национальные проекты</h2><a class="button button_link button_blue button_size_m  button_icon-right" href="/" attributes="{}"><span class="button_span">Подробнее</span>
+            <ion-icon class="icon" name="arrow-forward"></ion-icon></a>
+        </div>
+      </div>
+    </div>
+    <div class="people-care__slider">
+      <div class="slider"> <a class="slider__link-prev">
+          <ion-icon name="arrow-back-outline"></ion-icon></a><a class="slider__link-next">
+          <ion-icon name="arrow-forward-outline"></ion-icon></a></div>
+    </div>
+  </div>
+</div>
