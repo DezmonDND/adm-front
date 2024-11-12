194c454d58be8bf7f5a5f847cefb80bc38dc7a9a
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-11-12 17:52:51 +0300

    Сборка для PR

---
+++ bitrix/components/board/board-elem/template.html.php
@@ -0,0 +1,9 @@
+
+<div class="board-elem">
+  <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+    <div class="board-elem__info">
+      <div class="board-elem__info-date"><span>19.06.2023</span></div>
+      <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+    </div>
+  </div>
+</div>
---
+++ bitrix/components/board/main/template.html.php
@@ -0,0 +1,101 @@
+
+<div class="board"> 
+  <div class="board__content">
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О внесении изменения в постановление Администрации города от 28.11.2022 № 9334 «Об утверждении календарного плана физкультурных мероприятий и спортивных мероприятий муниципального образования городской округ Сургут Ханты-Мансийского автономного округа – Югры на 2023 год»</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О внесении изменения в постановление Администрации города от 28.11.2022 № 9334 «Об утверждении календарного плана физкультурных мероприятий и спортивных мероприятий муниципального образования городской округ Сургут Ханты-Мансийского автономного округа – Югры на 2023 год»</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О внесении изменения в постановление Администрации города от 28.11.2022 № 9334 «Об утверждении календарного плана физкультурных мероприятий и спортивных мероприятий муниципального образования городской округ Сургут Ханты-Мансийского автономного округа – Югры на 2023 год»</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О внесении изменения в постановление Администрации города от 28.11.2022 № 9334 «Об утверждении календарного плана физкультурных мероприятий и спортивных мероприятий муниципального образования городской округ Сургут Ханты-Мансийского автономного округа – Югры на 2023 год»</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="board-elem">
+      <div class="board-elem__container"><img class="board-elem__img" src="./resources/kirilenko_avatar.png"/>
+        <div class="board-elem__info">
+          <div class="board-elem__info-date"><span>19.06.2023</span></div>
+          <div class="board-elem__info-text"><span>О предоставлении субсидии субъекту малого и среднего предпринимательства</span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
