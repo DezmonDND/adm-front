94e9d4b9f077b189b5da7f902cb728e6e27da59f
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-11-14 00:01:19 +0300

    Сборка для PR

---
+++ bitrix/components/dm-deputies/dm-deputies-alphabet/template.html.php
@@ -0,0 +1,105 @@
+
+<div class="dm-deputies-alphabet">
+  <div class="dm-deputies-alphabet__content">
+    <div class="dm-deputies-alphabet__row">
+      <div class="dm-deputies-alphabet__letter"><span>А</span></div>
+      <div class="dm-deputies-alphabet__list">
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dm-deputies-alphabet__row">
+      <div class="dm-deputies-alphabet__letter"><span>Б</span></div>
+      <div class="dm-deputies-alphabet__list">
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dm-deputies-alphabet__row">
+      <div class="dm-deputies-alphabet__letter"><span>В</span></div>
+      <div class="dm-deputies-alphabet__list">
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dm-deputies-alphabet__row">
+      <div class="dm-deputies-alphabet__letter"><span>Г</span></div>
+      <div class="dm-deputies-alphabet__list">
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="dm-comitet-elem">
+          <div class="dm-comitet-elem__container"> <img class="dm-comitet-elem__img" src="./resources/kirilenko_avatar.png"/>
+            <div class="dm-comitet-elem__info">
+              <div class="dm-comitet-elem__info-surname"><span>Барсов</span></div>
+              <div class="dm-comitet-elem__info-name"><span>Евгений Вячеславович</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
