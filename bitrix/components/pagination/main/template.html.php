632359b372fa7791569a11b5cd9ae5136c5ec77b
unknown <denedoseikin@mail.ru>
2024-11-14 21:08:00 +0700

    fix: исправить замечания

--- bitrix/components/pagination/main/template.html.php
+++
@@ -1,6 +0,0 @@
-
-<div class="navigation"> <a class="navigation__link">
-    <ion-icon class="icon" name="arrow-back-outline"></ion-icon></a>
-  <div class="navigation__pages"><span class="navigation__page">1</span><span class="navigation__page">/</span><span class="navigation__page">2</span></div><a class="navigation__link">
-    <ion-icon class="icon" name="arrow-forward-outline"></ion-icon></a>
-</div>
