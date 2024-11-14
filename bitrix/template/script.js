0a055998feca6cb11ef1b1f82bc299442473da92
unknown <denedoseikin@mail.ru>
2024-11-14 22:35:47 +0700

    feat: сделать страницу Афиши

--- bitrix/template/script.js
+++ bitrix/template/script.js
@@ -160,6 +160,20 @@ function selectOption(element) {
   element.classList.toggle("select_multiple-option_selected");
 }
 
+function getLength() {
+  const textarea = document.querySelector(".textarea");
+  const counterCurrent = document.querySelector(".current");
+  const counterMax = document.querySelector(".max");
+
+  const textLength = textarea.value.length;
+  counterCurrent.textContent = textLength;
+  if (textLength > Number(counterMax.textContent)) {
+    textarea.style.borderColor = "#D10404";
+  } else {
+    textarea.style.borderColor = "";
+  }
+}
+
 $(document).ready(function () {
     $('.toggle-container').each(function () {
         // Находим основные элементы
@@ -220,17 +234,3 @@ $(document).ready(function () {
         });
     });
 });
-
-function getLength() {
-  const textarea = document.querySelector(".textarea");
-  const counterCurrent = document.querySelector(".current");
-  const counterMax = document.querySelector(".max");
-
-  const textLength = textarea.value.length;
-  counterCurrent.textContent = textLength;
-  if (textLength > Number(counterMax.textContent)) {
-    textarea.style.borderColor = "#D10404";
-  } else {
-    textarea.style.borderColor = "";
-  }
-}
