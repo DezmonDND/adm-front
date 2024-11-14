591caea5c1d9be8155183ff445c15001ddbf36a3
unknown <denedoseikin@mail.ru>
2024-11-14 16:59:56 +0700

    fix: исправить замечания Главная Дума

---
+++ bitrix/components/services/main/template.html.php
@@ -0,0 +1,41 @@
+
+<div class="full-width-container">
+  <div class="full-width-background">
+    <div class="full-width-content">                          
+      <div class="main-services"> 
+        <div class="main-title">
+          <div class="main-title__container">
+            <h1 class="main-title__title">Полезные сервисы для вас</h1>
+          </div>
+        </div>
+        <div class="main-services__container">
+          <div class="main-services__search">
+            <div class="main-services__search-content">
+              <h3 class="main-services__label">Поиск законопроектов</h3>
+              <form class="search search-main-services">
+                <input class="input  input-search input-search-l " placeholder="Поиск по наименованию или номеру" name="laws" value=""/>
+                <button class="search__button search__button_l" type="submit" onclick="getValues()">       </button>
+              </form>
+            </div>
+            <div class="main-services__search-content">       
+              <h3 class="main-services__label">Поиск правовых актов        </h3>
+              <form class="search search-main-services">
+                <input class="input  input-search input-search-l " placeholder="Поиск по наименованию или номеру" name="acts" value=""/>
+                <button class="search__button search__button_l" type="submit" onclick="getValues()">       </button>
+              </form>
+            </div>
+          </div>
+          <div class="main-services__buttons">
+            <h3 class="main-services__buttons-title">Связаться</h3>
+            <div class="main-services__buttons-container">
+              <button class="button button_blue main-services__button button_size_l   " onclick="console.log(1);"><span class="button_span">Электронное обращение</span>
+              </button>
+              <button class="button button_blue main-services__button button_size_l   " onclick="console.log(1);"><span class="button_span">Личный прием</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ bitrix/components/user-profile/main/template.html.php
@@ -0,0 +1,14 @@
+
+<div class="user-profile"> 
+  <button class="user-profile__button" onclick="changeAvatar()" type="button"> <img class="user-profile__avatar" src="/local/templates/surgut/img/user_avatar.png" alt="Аватар"/></button>
+  <div class="user-profile__info"> 
+    <h2 class="user-profile__name">Валентинов Георгий Викторович</h2><span class="user-profile_job">Должность     </span>
+  </div>
+</div>
+<div class="tabs">
+  <div class="tabs__container"><a class="tab-button "></a><a class="tab-button "></a><a class="tab-button "></a><a class="tab-button "></a><a class="tab-button "></a><a class="tab-button "></a><a class="tab-button "></a><a class="tab-button "></a>
+  </div>
+  <div class="slider"><a class="slider__link">
+      <ion-icon name="arrow-back-outline"></ion-icon></a><a class="slider__link">
+      <ion-icon name="arrow-forward-outline"> </ion-icon></a></div>
+</div>
