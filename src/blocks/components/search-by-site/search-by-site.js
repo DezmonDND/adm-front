40682ceafe43fb3da73491b23d3e77cc9ee8b167
unknown <denedoseikin@mail.ru>
2024-11-18 21:55:07 +0700

    feat: страница Поиск + модальное окно

---
+++ src/blocks/components/search-by-site/search-by-site.js
@@ -0,0 +1,7 @@
+function toggleCalendar() {
+    document.querySelector('.search-by-site__calendar').classList.toggle('search-by-site__calendar_active')
+}
+
+function toggleFilters() {
+    document.querySelector('.filters-popup').classList.toggle('filters-popup_open')
+}
