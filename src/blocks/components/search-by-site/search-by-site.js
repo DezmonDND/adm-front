b91ecf540878001d024aff6d494a54539d8aaa96
unknown <denedoseikin@mail.ru>
2024-11-19 14:56:54 +0700

    fix: заменить цвета на переменные

--- src/blocks/components/search-by-site/search-by-site.js
+++ src/blocks/components/search-by-site/search-by-site.js
@@ -1,18 +1,8 @@
-function toggleCalendar() {
-    document
-        .querySelector('.search-by-site__calendar')
-        .classList.toggle('search-by-site__calendar_active');
-}
+const calendar = document.querySelector('.search-by-site__calendar');
 
-document.addEventListener('click', function (e) {
-    console.log(e.target.classList);
-    
-    if (e.target.classList.contains('.search-by-site__calendar_active')) {
-        document
-            .querySelector('.search-by-site__calendar')
-            .classList.remove('search-by-site__calendar_active');
-    }
-});
+function toggleCalendar() {
+    calendar.classList.toggle('search-by-site__calendar_active');
+}
 
 function toggleFilters() {
     document.querySelector('.filters-popup').classList.toggle('filters-popup_open');
