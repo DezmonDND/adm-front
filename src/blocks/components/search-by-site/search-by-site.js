6d9764adae6d03bcb14dcc0cc73b23c084e68a8f
unknown <denedoseikin@mail.ru>
2024-11-19 14:16:16 +0700

    feat: изменить календарь

--- src/blocks/components/search-by-site/search-by-site.js
+++ src/blocks/components/search-by-site/search-by-site.js
@@ -1,7 +1,19 @@
 function toggleCalendar() {
-    document.querySelector('.search-by-site__calendar').classList.toggle('search-by-site__calendar_active')
+    document
+        .querySelector('.search-by-site__calendar')
+        .classList.toggle('search-by-site__calendar_active');
 }
 
+document.addEventListener('click', function (e) {
+    console.log(e.target.classList);
+    
+    if (e.target.classList.contains('.search-by-site__calendar_active')) {
+        document
+            .querySelector('.search-by-site__calendar')
+            .classList.remove('search-by-site__calendar_active');
+    }
+});
+
 function toggleFilters() {
-    document.querySelector('.filters-popup').classList.toggle('filters-popup_open')
+    document.querySelector('.filters-popup').classList.toggle('filters-popup_open');
 }
