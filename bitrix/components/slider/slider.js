7a204338e9825690c38cc6eede416c104a129529
unknown <denedoseikin@mail.ru>
2024-11-20 16:10:30 +0700

    feat: добавить Слайдер

--- bitrix/components/slider/slider.js
+++ bitrix/components/slider/slider.js
@@ -1,3 +1,16 @@
+// Главная новость
+
+$('.news-card-with-image__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    appendDots: $('.news-card-with-image__dots'),
+    adaptiveHeight: true,
+    variableWidth: false,
+});
+
 // Помогите городу стать лучше
 
 $('.help-city__content').slick({
@@ -5,6 +18,26 @@ $('.help-city__content').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+    ],
 });
 
 $('.slider__help-city-next').on('click', function () {
@@ -21,6 +54,26 @@ $('.important-inf__container').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     arrows: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+    ],
 });
 
 $('.slider__important-inf-next').on('click', function () {
@@ -37,6 +90,26 @@ $('.dm-city-projects__card-list').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     arrows: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+    ],
 });
 
 $('.slider__dm-city-projects-next').on('click', function () {
