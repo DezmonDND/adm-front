ef4b80daec95b904d5d5e974e6ae7ed08e347556
unknown <denedoseikin@mail.ru>
2024-11-25 22:48:24 +0700

    fix: исправить меню

--- bitrix/components/header/dm-second-menu/dm-second-menu.js
+++ bitrix/components/header/dm-second-menu/dm-second-menu.js
@@ -1,28 +1,45 @@
 function toggleSecondMenu(event) {
-    const allMenus = document.querySelectorAll('.dm-second-menu');
-    const allButtons = document.querySelectorAll('.header__link');
-
-    allMenus.forEach((menu) => menu.classList.remove('dm-second-menu_active'));
-    allButtons.forEach((button) => button.classList.remove('header__link_active'));
-
-    let secondMenu;
+    const activeMenu = document.querySelector('.dm-second-menu_active');
+    const activeLink = document.querySelector('.header__link_active');
     const button = event.target;
-
-    button.classList.add('header__link_active');
+    let newMenu;
 
     if (event.target.textContent === 'О Думе города') {
-        secondMenu = document.querySelector('.dm-second-menu_about');
+        newMenu = document.querySelector('.dm-second-menu_about');
     } else if (event.target.textContent === 'Состав и структура') {
-        secondMenu = document.querySelector('.dm-second-menu_struct');
+        newMenu = document.querySelector('.dm-second-menu_struct');
     } else if (event.target.textContent === 'Документы') {
-        secondMenu = document.querySelector('.dm-second-menu_doc');
+        newMenu = document.querySelector('.dm-second-menu_doc');
     } else if (event.target.textContent === 'Деятельность') {
-        secondMenu = document.querySelector('.dm-second-menu_actions');
-    } else {
-        ('');
+        newMenu = document.querySelector('.dm-second-menu_actions');
     }
 
-    if (secondMenu) {
-        secondMenu.classList.add('dm-second-menu_active');
+    if (newMenu) {
+        if (activeMenu && activeMenu === newMenu) {
+            activeMenu.classList.remove('dm-second-menu_active');
+            activeLink.classList.remove('header__link_active');
+        } else if (activeMenu && activeMenu !== newMenu) {
+            activeMenu.classList.remove('dm-second-menu_active');
+            activeLink.classList.remove('header__link_active');
+            button.classList.add('header__link_active');
+            newMenu.classList.add('dm-second-menu_active');
+        } else {
+            button.classList.add('header__link_active');
+            newMenu.classList.add('dm-second-menu_active');
+        }
     }
 }
+
+document.body.addEventListener('click', (event) => {
+    if (!event.target.closest('.header__link')) {
+        const dropdowns = document.querySelectorAll('.dm-second-menu');
+        dropdowns.forEach((dropdown) => {
+            if (
+                dropdown.classList.contains('dm-second-menu_active') &&
+                !dropdown.contains(event.target)
+            ) {
+                dropdown.classList.remove('dm-second-menu_active');
+            }
+        });
+    }
+});
