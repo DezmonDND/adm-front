5b36304012541d31638cac2a7b412d76123ee334
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-11-25 16:28:14 +0300

    Сборка

--- bitrix/components/honor-board/board-detail/template.html.php
+++ bitrix/components/honor-board/board-detail/template.html.php
@@ -5,7 +5,7 @@
       <div class="board-detail__desc-info"><span class="board-detail__date">28.06.2024</span>
         <p class="board-detail__about">Победитель Всероссийского конкурса профессионального мастерства «Лучший студенческий педагогический отряд» в номинации «Вожатские команды загородных организаций отдыха и оздоровления детей, областных и федеральных детских центров», 2020 год, III Всероссийского конкурса студенческих программ, проектов и практик воспитания в общеобразовательных организациях «Вожатые – школе», проект «Твой Skills», 2021 год.</p>
       </div>
-      <button class="button button_white   button_icon-left " onclick="anotherFunction()"><span class="button_span">Поделиться</span>
+      <button class="button button_white   button_icon-left " onclick="anotherFunction()" title=""><span class="button_span">Поделиться</span>
         <ion-icon class="icon" name="link-outline"></ion-icon>
       </button>
     </div>
