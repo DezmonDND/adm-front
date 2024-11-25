c14311c1d6d7f6a6f9b0f6b8ecf6cb25e8212391
unknown <denedoseikin@mail.ru>
2024-11-26 01:14:01 +0700

    fix: исправить изобрадения для Почетные граждане

--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -201,20 +201,6 @@ function selectOption(element) {
   element.classList.toggle("select_multiple-option_selected");
 }
 
-function getLength() {
-  const textarea = document.querySelector(".textarea");
-  const counterCurrent = document.querySelector(".current");
-  const counterMax = document.querySelector(".max");
-
-  const textLength = textarea.value.length;
-  counterCurrent.textContent = textLength;
-  if (textLength > Number(counterMax.textContent)) {
-    textarea.style.borderColor = "#D10404";
-  } else {
-    textarea.style.borderColor = "";
-  }
-}
-
 $(document).ready(function () {
     $('.toggle-container').each(function () {
         // Находим основные элементы
@@ -275,3 +261,17 @@ $(document).ready(function () {
         });
     });
 });
+
+function getLength() {
+  const textarea = document.querySelector(".textarea");
+  const counterCurrent = document.querySelector(".current");
+  const counterMax = document.querySelector(".max");
+
+  const textLength = textarea.value.length;
+  counterCurrent.textContent = textLength;
+  if (textLength > Number(counterMax.textContent)) {
+    textarea.style.borderColor = "#D10404";
+  } else {
+    textarea.style.borderColor = "";
+  }
+}
