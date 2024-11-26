ae39923395a5d9f0a07377610865d508b9fa2944
unknown <denedoseikin@mail.ru>
2024-11-26 19:24:54 +0700

    fix: исправить замечания

--- src/blocks/components/afisha/event-full-desc/event-full-desc.js
+++ src/blocks/components/afisha/event-full-desc/event-full-desc.js
@@ -1,10 +1,18 @@
-$('.afisha-event__banner-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    adaptiveHeight: true,
-    variableWidth: false,
+$(document).ready(function () {
+    $('.afisha-event__banner-list').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        adaptiveHeight: true,
+        variableWidth: false,
+        init: function (event, slick) {
+            updateSlideCount(slick);
+        },
+        beforeChange: function (event, slick, currentSlide, nextSlide) {
+            updateSlideCount(slick);
+        },
+    });
 });
 
 $('.slider__afisha-event-next').on('click', function () {
@@ -13,3 +21,10 @@ $('.slider__afisha-event-next').on('click', function () {
 $('.slider__afisha-event-prev').on('click', function () {
     $('.afisha-event__banner-list').slick('slickPrev');
 });
+
+function updateSlideCount(slick) {
+    console.log("Slick object:", slick);
+    const currentSlide = slick.slickCurrentSlide;
+    const totalSlides = slick.slideCount;
+    $('.afisha-event__comment').text(`${currentSlide + 1}/${totalSlides}`);
+}
