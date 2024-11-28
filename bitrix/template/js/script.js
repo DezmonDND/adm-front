75875ca2439678e91a67ca2c515b37cad6c7356e
unknown <denedoseikin@mail.ru>
2024-11-28 22:51:53 +0700

    fix: исправить замечания

--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -191,16 +191,6 @@ function updateMultiSelectValue(e) {
     getValues(e);
 }
 
-function openSelect() {
-  const selectMenu = document.querySelector(".select_multiple-menu");
-  selectMenu.style.display =
-    selectMenu.style.display === "none" ? "flex" : "none";
-}
-
-function selectOption(element) {
-  element.classList.toggle("select_multiple-option_selected");
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         const socialShareButtons = document.querySelectorAll('.social_share');
@@ -293,19 +283,39 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 500); 
 });
 
-function getLength() {
-  const textarea = document.querySelector(".textarea");
-  const counterCurrent = document.querySelector(".current");
-  const counterMax = document.querySelector(".max");
-
-  const textLength = textarea.value.length;
-  counterCurrent.textContent = textLength;
-  if (textLength > Number(counterMax.textContent)) {
-    textarea.style.borderColor = "#D10404";
-  } else {
-    textarea.style.borderColor = "";
-  }
+function openSelect() {
+  const selectMenu = document.querySelector(".select_multiple-menu");
+  selectMenu.style.display =
+    selectMenu.style.display === "none" ? "flex" : "none";
 }
+
+function selectOption(element) {
+  element.classList.toggle("select_multiple-option_selected");
+}
+
+function getLength(event) {
+    const textarea = event.target;
+    console.log(event.target);
+
+    const counterCurrent = textarea.parentNode.querySelector('.current');
+    const counterMax = textarea.parentNode.querySelector('.max');
+
+    if (counterCurrent && counterMax) {
+        const textLength = textarea.value.length;
+        counterCurrent.textContent = textLength;
+        if (textLength > Number(counterMax.textContent)) {
+            textarea.style.borderColor = '#D10404';
+        } else {
+            textarea.style.borderColor = '';
+        }
+    }
+}
+
+const textareas = document.querySelectorAll('.textarea');
+
+textareas.forEach((textarea) => {
+    textarea.addEventListener('input', getLength);
+});
 
 $(document).ready(function () {
     $('.toggle-container').each(function () {
--- src/blocks/libs/textarea/textarea.js
+++ src/blocks/libs/textarea/textarea.js
@@ -1,13 +1,23 @@
-function getLength() {
-  const textarea = document.querySelector(".textarea");
-  const counterCurrent = document.querySelector(".current");
-  const counterMax = document.querySelector(".max");
+function getLength(event) {
+    const textarea = event.target;
+    console.log(event.target);
 
-  const textLength = textarea.value.length;
-  counterCurrent.textContent = textLength;
-  if (textLength > Number(counterMax.textContent)) {
-    textarea.style.borderColor = "#D10404";
-  } else {
-    textarea.style.borderColor = "";
-  }
+    const counterCurrent = textarea.parentNode.querySelector('.current');
+    const counterMax = textarea.parentNode.querySelector('.max');
+
+    if (counterCurrent && counterMax) {
+        const textLength = textarea.value.length;
+        counterCurrent.textContent = textLength;
+        if (textLength > Number(counterMax.textContent)) {
+            textarea.style.borderColor = '#D10404';
+        } else {
+            textarea.style.borderColor = '';
+        }
+    }
 }
+
+const textareas = document.querySelectorAll('.textarea');
+
+textareas.forEach((textarea) => {
+    textarea.addEventListener('input', getLength);
+});
