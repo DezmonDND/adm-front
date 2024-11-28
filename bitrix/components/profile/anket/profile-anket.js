6c08492c6328104002848961a8774ed935cdcd45
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-11-28 17:46:51 +0300

    Сборка

--- bitrix/components/profile/anket/profile-anket.js
+++ bitrix/components/profile/anket/profile-anket.js
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const addButton = document.querySelector('.add-event-btn');
     const fieldsetContainer = document.querySelector('.fieldset-container');
-    console.log(fieldsetContainer)
-    let fieldCounter = 1;
+    let fieldCounter = 2;
+
+    const updateFieldNames = () => {
+        const fieldsets = fieldsetContainer.querySelectorAll('.fieldset');
+        fieldCounter = fieldsets.length + 1;
+        fieldsets.forEach((fieldset, index) => {
+            const input = fieldset.querySelector('.input');
+            input.name = `answer${index + 1}`;
+        });
+    };
 
     addButton.addEventListener('click', () => {
-        const newFieldset = document.createElement('div');
+        const newFieldset = document.createElement('fieldset');
         newFieldset.classList.add('fieldset');
         newFieldset.innerHTML = `
-            <fieldset class="fieldset ">
                 <div class="fieldset__title-remove">
-                    <h3 class="fieldset__title ">Название мероприятия и предполагаемая сумма денег </h3>
+                    <h3 class="fieldset__title">Название мероприятия и предполагаемая сумма денег</h3>
                     <ion-icon class="fieldset__title-remove-btn md hydrated" name="close-outline" role="img"></ion-icon>
-                </div><div class="fieldset__answers">
-                <input class="input  input-interview-full" placeholder="Введите название мероприятия и сумму (руб, с двумя знаками после запятой)" name="answer${fieldCounter}" value=""></div></fieldset>
+                </div>
+                <div class="fieldset__answers">
+                    <input class="input input-interview-full" placeholder="Введите название мероприятия и сумму (руб, с двумя знаками после запятой)" name="answer${fieldCounter}" value="">
+                </div>
         `;
         fieldCounter++;
         fieldsetContainer.appendChild(newFieldset);
@@ -21,7 +30,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const removeButton = newFieldset.querySelector('.fieldset__title-remove-btn');
         removeButton.addEventListener('click', () => {
             newFieldset.remove();
-            fieldCounter--;
+            updateFieldNames(); 
         });
     });
+
+    updateFieldNames();
 });
--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -39,81 +39,6 @@ $(document).ready(function () {
     });
 });
 
-const msDropdownList = document.querySelector('.multi-select__dropdown');
-const msDropdownItems = document.querySelectorAll('.multi-select__dropdown-item');
-const multiSelect = document.querySelector('input[name="multiSelect"]');
-const msInput = document.querySelector('.multi-select__input');
-const msChose = document.querySelector('.multi-select__chose');
-let visibleDropdownItems;
-let counter = -1;
-let selectedValues = [];
-
-msChose &&
-    msChose.addEventListener('click', (event) => {
-        if (!event.target.closest('.multi-select__chose-item')) {
-            msDropdownList.classList.remove('multi-select__dropdown_hidden');
-        }
-    });
-
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('.multi-select') && msDropdownList) {
-        msDropdownList.classList.add('multi-select__dropdown_hidden');
-    }
-
-    if (event.target.closest('.multi-select__chose-item')) {
-        searchSelectedElement(event.target.textContent);
-    }
-});
-
-msDropdownList &&
-    msDropdownList.addEventListener('click', (event) => {
-        if (event.target.classList.contains('multi-select__dropdown-item_chose')) {
-            searchSelectedElement(event.target.textContent);
-        } else if (event.target.classList.contains('multi-select__dropdown-item')) {
-            createNewElement('li', ['multi-select__chose-item'], event, msChose);
-            selectedValues.push(event.target.dataset.value);
-            updateMultiSelectValue();
-        }
-        msInput.value = '';
-        msInput.focus();
-    });
-
-function createNewElement(tag, styles, event, parent) {
-    const newElement = document.createElement(tag);
-    newElement.classList.add(...styles);
-    newElement.textContent = event.target.textContent;
-    parent.prepend(newElement);
-    event.target.classList.add('multi-select__dropdown-item_chose');
-}
-
-function searchSelectedElement(text) {
-    msDropdownItems.forEach((item) => {
-        if (text.toLowerCase() === item.textContent.toLowerCase()) {
-            item.classList.remove('multi-select__dropdown-item_chose');
-            deleteElement(text);
-            const index = selectedValues.indexOf(item.dataset.value);
-            if (index > -1) {
-                selectedValues.splice(index, 1);
-            }
-            updateMultiSelectValue();
-        }
-    });
-}
-
-function deleteElement(text) {
-    const msChoseItems = document.querySelectorAll('.multi-select__chose-item');
-    msChoseItems.forEach((item) => {
-        if (text.toLowerCase() === item.textContent.toLowerCase()) {
-            item.remove();
-        }
-    });
-}
-
-function updateMultiSelectValue(e) {
-    multiSelect.value = selectedValues.join(', ');
-    getValues(e);
-}
-
 $(document).ready(function () {
     // Инициализация всех popup-блоков
     $('.popup-fade').each(function () {
@@ -191,6 +116,81 @@ $(document).ready(function () {
     });
 });
 
+const msDropdownList = document.querySelector('.multi-select__dropdown');
+const msDropdownItems = document.querySelectorAll('.multi-select__dropdown-item');
+const multiSelect = document.querySelector('input[name="multiSelect"]');
+const msInput = document.querySelector('.multi-select__input');
+const msChose = document.querySelector('.multi-select__chose');
+let visibleDropdownItems;
+let counter = -1;
+let selectedValues = [];
+
+msChose &&
+    msChose.addEventListener('click', (event) => {
+        if (!event.target.closest('.multi-select__chose-item')) {
+            msDropdownList.classList.remove('multi-select__dropdown_hidden');
+        }
+    });
+
+document.addEventListener('click', (event) => {
+    if (!event.target.closest('.multi-select') && msDropdownList) {
+        msDropdownList.classList.add('multi-select__dropdown_hidden');
+    }
+
+    if (event.target.closest('.multi-select__chose-item')) {
+        searchSelectedElement(event.target.textContent);
+    }
+});
+
+msDropdownList &&
+    msDropdownList.addEventListener('click', (event) => {
+        if (event.target.classList.contains('multi-select__dropdown-item_chose')) {
+            searchSelectedElement(event.target.textContent);
+        } else if (event.target.classList.contains('multi-select__dropdown-item')) {
+            createNewElement('li', ['multi-select__chose-item'], event, msChose);
+            selectedValues.push(event.target.dataset.value);
+            updateMultiSelectValue();
+        }
+        msInput.value = '';
+        msInput.focus();
+    });
+
+function createNewElement(tag, styles, event, parent) {
+    const newElement = document.createElement(tag);
+    newElement.classList.add(...styles);
+    newElement.textContent = event.target.textContent;
+    parent.prepend(newElement);
+    event.target.classList.add('multi-select__dropdown-item_chose');
+}
+
+function searchSelectedElement(text) {
+    msDropdownItems.forEach((item) => {
+        if (text.toLowerCase() === item.textContent.toLowerCase()) {
+            item.classList.remove('multi-select__dropdown-item_chose');
+            deleteElement(text);
+            const index = selectedValues.indexOf(item.dataset.value);
+            if (index > -1) {
+                selectedValues.splice(index, 1);
+            }
+            updateMultiSelectValue();
+        }
+    });
+}
+
+function deleteElement(text) {
+    const msChoseItems = document.querySelectorAll('.multi-select__chose-item');
+    msChoseItems.forEach((item) => {
+        if (text.toLowerCase() === item.textContent.toLowerCase()) {
+            item.remove();
+        }
+    });
+}
+
+function updateMultiSelectValue(e) {
+    multiSelect.value = selectedValues.join(', ');
+    getValues(e);
+}
+
 function openSelect() {
   const selectMenu = document.querySelector(".select_multiple-menu");
   selectMenu.style.display =
@@ -199,20 +199,6 @@ function openSelect() {
 
 function selectOption(element) {
   element.classList.toggle("select_multiple-option_selected");
-}
-
-function getLength() {
-  const textarea = document.querySelector(".textarea");
-  const counterCurrent = document.querySelector(".current");
-  const counterMax = document.querySelector(".max");
-
-  const textLength = textarea.value.length;
-  counterCurrent.textContent = textLength;
-  if (textLength > Number(counterMax.textContent)) {
-    textarea.style.borderColor = "#D10404";
-  } else {
-    textarea.style.borderColor = "";
-  }
 }
 
 $(document).ready(function () {
@@ -275,3 +261,17 @@ $(document).ready(function () {
         });
     });
 });
+
+function getLength() {
+  const textarea = document.querySelector(".textarea");
+  const counterCurrent = document.querySelector(".current");
+  const counterMax = document.querySelector(".max");
+
+  const textLength = textarea.value.length;
+  counterCurrent.textContent = textLength;
+  if (textLength > Number(counterMax.textContent)) {
+    textarea.style.borderColor = "#D10404";
+  } else {
+    textarea.style.borderColor = "";
+  }
+}
