8e17312ae2e71623a7f92a52c232ac0e3bba8a79
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-11-28 17:08:51 +0300

    Сборка

--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -1,77 +1,43 @@
-const msDropdownList = document.querySelector('.multi-select__dropdown');
-const msDropdownItems = document.querySelectorAll('.multi-select__dropdown-item');
-const multiSelect = document.querySelector('input[name="multiSelect"]');
-const msInput = document.querySelector('.multi-select__input');
-const msChose = document.querySelector('.multi-select__chose');
-let visibleDropdownItems;
-let counter = -1;
-let selectedValues = [];
+// Находим все элементы с классом `content-card`
+$(document).ready(function () {
+    document.querySelectorAll('.content-card').forEach((card) => {
+        // Создаем контейнер внутри карточки
+        const container = document.createElement('div');
+        container.className = 'content-card__container';
 
-msChose &&
-    msChose.addEventListener('click', (event) => {
-        if (!event.target.closest('.multi-select__chose-item')) {
-            msDropdownList.classList.remove('multi-select__dropdown_hidden');
+        // Перемещаем текущие дочерние элементы в контейнер
+        while (card.firstChild) {
+            container.appendChild(card.firstChild);
         }
+
+    // Создаем ссылку с кнопкой "Поделиться"
+    const shareButton = document.createElement('button');
+    shareButton.className =
+        'button button_link button_white button_size_m button_icon-left social_share';
+    shareButton.href = '/';
+    shareButton.setAttribute('attributes', '{}');
+
+        // Создаем содержимое кнопки
+        const buttonSpan = document.createElement('span');
+        buttonSpan.className = 'button_span';
+        buttonSpan.textContent = 'Поделиться';
+
+        const icon = document.createElement('ion-icon');
+        icon.className = 'icon md hydrated';
+        icon.setAttribute('name', 'link-outline');
+        icon.setAttribute('role', 'img');
+
+        // Собираем структуру кнопки
+        shareButton.appendChild(buttonSpan);
+        shareButton.appendChild(icon);
+
+        // Добавляем кнопку в контейнер
+        container.appendChild(shareButton);
+
+        // Добавляем контейнер в карточку
+        card.appendChild(container);
     });
-
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('.multi-select') && msDropdownList) {
-        msDropdownList.classList.add('multi-select__dropdown_hidden');
-    }
-
-    if (event.target.closest('.multi-select__chose-item')) {
-        searchSelectedElement(event.target.textContent);
-    }
 });
-
-msDropdownList &&
-    msDropdownList.addEventListener('click', (event) => {
-        if (event.target.classList.contains('multi-select__dropdown-item_chose')) {
-            searchSelectedElement(event.target.textContent);
-        } else if (event.target.classList.contains('multi-select__dropdown-item')) {
-            createNewElement('li', ['multi-select__chose-item'], event, msChose);
-            selectedValues.push(event.target.dataset.value);
-            updateMultiSelectValue();
-        }
-        msInput.value = '';
-        msInput.focus();
-    });
-
-function createNewElement(tag, styles, event, parent) {
-    const newElement = document.createElement(tag);
-    newElement.classList.add(...styles);
-    newElement.textContent = event.target.textContent;
-    parent.prepend(newElement);
-    event.target.classList.add('multi-select__dropdown-item_chose');
-}
-
-function searchSelectedElement(text) {
-    msDropdownItems.forEach((item) => {
-        if (text.toLowerCase() === item.textContent.toLowerCase()) {
-            item.classList.remove('multi-select__dropdown-item_chose');
-            deleteElement(text);
-            const index = selectedValues.indexOf(item.dataset.value);
-            if (index > -1) {
-                selectedValues.splice(index, 1);
-            }
-            updateMultiSelectValue();
-        }
-    });
-}
-
-function deleteElement(text) {
-    const msChoseItems = document.querySelectorAll('.multi-select__chose-item');
-    msChoseItems.forEach((item) => {
-        if (text.toLowerCase() === item.textContent.toLowerCase()) {
-            item.remove();
-        }
-    });
-}
-
-function updateMultiSelectValue(e) {
-    multiSelect.value = selectedValues.join(', ');
-    getValues(e);
-}
 
 $(document).ready(function () {
     // Инициализация всех popup-блоков
