14d2fa5f9d3d37df181c513b88d43402019a3fa1
unknown <denedoseikin@mail.ru>
2024-11-29 20:18:52 +0700

    feat: создать страницу Виртуальная приемная для Думы

--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -205,38 +205,6 @@ function updateMultiSelectValue(e) {
     getValues(e);
 }
 
-function openSelect() {
-  const selectMenu = document.querySelector(".select_multiple-menu");
-  selectMenu.style.display =
-    selectMenu.style.display === "none" ? "flex" : "none";
-}
-
-function selectOption(element) {
-  element.classList.toggle("select_multiple-option_selected");
-}
-
-function getLength(event) {
-    const textarea = event.target;
-    const counterCurrent = textarea.parentNode.querySelector('.current');
-    const counterMax = textarea.parentNode.querySelector('.max');
-
-    if (counterCurrent && counterMax) {
-        const textLength = textarea.value.length;
-        counterCurrent.textContent = textLength;
-        if (textLength > Number(counterMax.textContent)) {
-            textarea.style.borderColor = '#D10404';
-        } else {
-            textarea.style.borderColor = '';
-        }
-    }
-}
-
-const textareas = document.querySelectorAll('.textarea');
-
-textareas.forEach((textarea) => {
-    textarea.addEventListener('input', getLength);
-});
-
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         const socialShareButtons = document.querySelectorAll('.social_share');
@@ -327,6 +295,38 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }, 500); 
+});
+
+function openSelect() {
+  const selectMenu = document.querySelector(".select_multiple-menu");
+  selectMenu.style.display =
+    selectMenu.style.display === "none" ? "flex" : "none";
+}
+
+function selectOption(element) {
+  element.classList.toggle("select_multiple-option_selected");
+}
+
+function getLength(event) {
+    const textarea = event.target;
+    const counterCurrent = textarea.parentNode.querySelector('.current');
+    const counterMax = textarea.parentNode.querySelector('.max');
+
+    if (counterCurrent && counterMax) {
+        const textLength = textarea.value.length;
+        counterCurrent.textContent = textLength;
+        if (textLength > Number(counterMax.textContent)) {
+            textarea.style.borderColor = '#D10404';
+        } else {
+            textarea.style.borderColor = '';
+        }
+    }
+}
+
+const textareas = document.querySelectorAll('.textarea');
+
+textareas.forEach((textarea) => {
+    textarea.addEventListener('input', getLength);
 });
 
 $(document).ready(function () {
