72a9a88b22855f1df3ad6d756f96c60f75ff133b
unknown <denedoseikin@mail.ru>
2024-11-29 22:53:20 +0700

    fix: различные исправления

--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -116,6 +116,16 @@ $(document).ready(function () {
     });
 });
 
+function openSelect() {
+  const selectMenu = document.querySelector(".select_multiple-menu");
+  selectMenu.style.display =
+    selectMenu.style.display === "none" ? "flex" : "none";
+}
+
+function selectOption(element) {
+  element.classList.toggle("select_multiple-option_selected");
+}
+
 const msDropdownList = document.querySelector('.multi-select__dropdown');
 const msDropdownItems = document.querySelectorAll('.multi-select__dropdown-item');
 const multiSelect = document.querySelector('input[name="multiSelect"]');
@@ -189,16 +199,6 @@ function deleteElement(text) {
 function updateMultiSelectValue(e) {
     multiSelect.value = selectedValues.join(', ');
     getValues(e);
-}
-
-function openSelect() {
-  const selectMenu = document.querySelector(".select_multiple-menu");
-  selectMenu.style.display =
-    selectMenu.style.display === "none" ? "flex" : "none";
-}
-
-function selectOption(element) {
-  element.classList.toggle("select_multiple-option_selected");
 }
 
 document.addEventListener('DOMContentLoaded', () => {
