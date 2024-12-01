a7128d7104e8cdda12e07c4f96f8c5ca01ef2aef
unknown <denedoseikin@mail.ru>
2024-12-01 18:36:34 +0700

    fix: отображать выбранные даты в тексте кнопки

--- src/blocks/components/chronicle/calendar/calendar.js
+++ src/blocks/components/chronicle/calendar/calendar.js
@@ -22,16 +22,22 @@ const options = {
                 };
                 newDates = dates;
 
+                const button = document.querySelector('.search-by-site__button');
+
                 if (from && to) {
                     from.value = new Date(dates.from).toLocaleDateString();
                     to.value = new Date(dates.to).toLocaleDateString();
-                    console.log(from.value);
-                    console.log(to.value);
+
+                    if (from.value !== 'Invalid Date' && from.value !== 'Invalid Date') {
+                        button.textContent = `Период с ${from.value} по ${to.value}`;
+                    } else {
+                        button.textContent = `Период публикации`;
+                    }
+                } else {
                 }
             } else {
                 console.log('Нет выбранных дат');
             }
-            console.log(newDates);
         },
     },
     CSSClasses: {
