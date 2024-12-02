77bce107c96f7c61a4267f6a4c39b6e0f3a26802
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-12-02 15:23:53 +0300

    Сборка

--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -1,58 +1,3 @@
-// Находим все элементы с классом `content-card`
-$(document).ready(function () {
-    document.querySelectorAll('.content-card').forEach((card) => {
-
-        const fullWidthContainer = document.createElement('div');
-        fullWidthContainer.className = 'full-width-container';
-
-        const fullWidthBackground = document.createElement('div');
-        fullWidthBackground.className = 'full-width-background';
-
-        const fullWidthContent = document.createElement('div');
-        fullWidthContent.className = 'full-width-content';
-
-        card.parentNode.insertBefore(fullWidthContainer, card); 
-        fullWidthContent.appendChild(card); 
-        fullWidthBackground.appendChild(fullWidthContent); // 
-        fullWidthContainer.appendChild(fullWidthBackground);
-        // Создаем контейнер внутри карточки
-        const container = document.createElement('div');
-        container.className = 'content-card__container';
-
-        // Перемещаем текущие дочерние элементы в контейнер
-        while (card.firstChild) {
-            container.appendChild(card.firstChild);
-        }
-
-    // Создаем ссылку с кнопкой "Поделиться"
-    const shareButton = document.createElement('button');
-    shareButton.className =
-        'button button_link button_white button_size_m button_icon-left social_share';
-    shareButton.href = '/';
-    shareButton.setAttribute('attributes', '{}');
-
-        // Создаем содержимое кнопки
-        const buttonSpan = document.createElement('span');
-        buttonSpan.className = 'button_span';
-        buttonSpan.textContent = 'Поделиться';
-
-        const icon = document.createElement('ion-icon');
-        icon.className = 'icon md hydrated';
-        icon.setAttribute('name', 'link-outline');
-        icon.setAttribute('role', 'img');
-
-        // Собираем структуру кнопки
-        shareButton.appendChild(buttonSpan);
-        shareButton.appendChild(icon);
-
-        // Добавляем кнопку в контейнер
-        container.appendChild(shareButton);
-
-        // Добавляем контейнер в карточку
-        card.appendChild(container);
-    });
-});
-
 $(document).ready(function () {
     // Инициализация всех popup-блоков
     $('.popup-fade').each(function () {
@@ -205,6 +150,16 @@ function updateMultiSelectValue(e) {
     getValues(e);
 }
 
+function openSelect() {
+  const selectMenu = document.querySelector(".select_multiple-menu");
+  selectMenu.style.display =
+    selectMenu.style.display === "none" ? "flex" : "none";
+}
+
+function selectOption(element) {
+  element.classList.toggle("select_multiple-option_selected");
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         const socialShareButtons = document.querySelectorAll('.social_share');
@@ -297,15 +252,82 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 500); 
 });
 
-function openSelect() {
-  const selectMenu = document.querySelector(".select_multiple-menu");
-  selectMenu.style.display =
-    selectMenu.style.display === "none" ? "flex" : "none";
+function getLength(event) {
+    const textarea = event.target;
+    const counterCurrent = textarea.parentNode.querySelector('.current');
+    const counterMax = textarea.parentNode.querySelector('.max');
+
+    if (counterCurrent && counterMax) {
+        const textLength = textarea.value.length;
+        counterCurrent.textContent = textLength;
+        if (textLength > Number(counterMax.textContent)) {
+            textarea.style.borderColor = '#D10404';
+        } else {
+            textarea.style.borderColor = '';
+        }
+    }
 }
 
-function selectOption(element) {
-  element.classList.toggle("select_multiple-option_selected");
-}
+const textareas = document.querySelectorAll('.textarea');
+
+textareas.forEach((textarea) => {
+    textarea.addEventListener('input', getLength);
+});
+
+// Находим все элементы с классом `content-card`
+$(document).ready(function () {
+    document.querySelectorAll('.content-card').forEach((card) => {
+
+        const fullWidthContainer = document.createElement('div');
+        fullWidthContainer.className = 'full-width-container';
+
+        const fullWidthBackground = document.createElement('div');
+        fullWidthBackground.className = 'full-width-background';
+
+        const fullWidthContent = document.createElement('div');
+        fullWidthContent.className = 'full-width-content';
+
+        card.parentNode.insertBefore(fullWidthContainer, card); 
+        fullWidthContent.appendChild(card); 
+        fullWidthBackground.appendChild(fullWidthContent); // 
+        fullWidthContainer.appendChild(fullWidthBackground);
+        // Создаем контейнер внутри карточки
+        const container = document.createElement('div');
+        container.className = 'content-card__container';
+
+        // Перемещаем текущие дочерние элементы в контейнер
+        while (card.firstChild) {
+            container.appendChild(card.firstChild);
+        }
+
+    // Создаем ссылку с кнопкой "Поделиться"
+    const shareButton = document.createElement('button');
+    shareButton.className =
+        'button button_link button_white button_size_m button_icon-left social_share';
+    shareButton.href = '/';
+    shareButton.setAttribute('attributes', '{}');
+
+        // Создаем содержимое кнопки
+        const buttonSpan = document.createElement('span');
+        buttonSpan.className = 'button_span';
+        buttonSpan.textContent = 'Поделиться';
+
+        const icon = document.createElement('ion-icon');
+        icon.className = 'icon md hydrated';
+        icon.setAttribute('name', 'link-outline');
+        icon.setAttribute('role', 'img');
+
+        // Собираем структуру кнопки
+        shareButton.appendChild(buttonSpan);
+        shareButton.appendChild(icon);
+
+        // Добавляем кнопку в контейнер
+        container.appendChild(shareButton);
+
+        // Добавляем контейнер в карточку
+        card.appendChild(container);
+    });
+});
 
 $(document).ready(function () {
     $('.toggle-container').each(function () {
@@ -366,26 +388,4 @@ $(document).ready(function () {
             header.find('.toggle-arrow').toggleClass('rotated');
         });
     });
-});
-
-function getLength(event) {
-    const textarea = event.target;
-    const counterCurrent = textarea.parentNode.querySelector('.current');
-    const counterMax = textarea.parentNode.querySelector('.max');
-
-    if (counterCurrent && counterMax) {
-        const textLength = textarea.value.length;
-        counterCurrent.textContent = textLength;
-        if (textLength > Number(counterMax.textContent)) {
-            textarea.style.borderColor = '#D10404';
-        } else {
-            textarea.style.borderColor = '';
-        }
-    }
-}
-
-const textareas = document.querySelectorAll('.textarea');
-
-textareas.forEach((textarea) => {
-    textarea.addEventListener('input', getLength);
 });
