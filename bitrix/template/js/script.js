95a015a09cf1b973c26d484663e07d3a5e82105c
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-12-02 18:39:29 +0300

    Сборка

--- bitrix/template/js/script.js
+++ bitrix/template/js/script.js
@@ -1,58 +1,3 @@
-// Находим все элементы с классом `content-card`
-$(document).ready(function () {
-    document.querySelectorAll('.content-card').forEach((card) => {
-
-        const fullWidthContainer = document.createElement('div');
-        fullWidthContainer.className = 'full-width-container';
-
-        const fullWidthBackground = document.createElement('div');
-        fullWidthBackground.className = 'full-width-background';
-
-        const fullWidthContent = document.createElement('div');
-        fullWidthContent.className = 'full-width-content';
-
-        card.parentNode.insertBefore(fullWidthContainer, card); 
-        fullWidthContent.appendChild(card); 
-        fullWidthBackground.appendChild(fullWidthContent); // 
-        fullWidthContainer.appendChild(fullWidthBackground);
-        // Создаем контейнер внутри карточки
-        const container = document.createElement('div');
-        container.className = 'content-card__container';
-
-        // Перемещаем текущие дочерние элементы в контейнер
-        while (card.firstChild) {
-            container.appendChild(card.firstChild);
-        }
-
-    // Создаем ссылку с кнопкой "Поделиться"
-    const shareButton = document.createElement('button');
-    shareButton.className =
-        'button button_link button_white button_size_m button_icon-left social_share';
-    shareButton.href = '/';
-    shareButton.setAttribute('attributes', '{}');
-
-        // Создаем содержимое кнопки
-        const buttonSpan = document.createElement('span');
-        buttonSpan.className = 'button_span';
-        buttonSpan.textContent = 'Поделиться';
-
-        const icon = document.createElement('ion-icon');
-        icon.className = 'icon md hydrated';
-        icon.setAttribute('name', 'link-outline');
-        icon.setAttribute('role', 'img');
-
-        // Собираем структуру кнопки
-        shareButton.appendChild(buttonSpan);
-        shareButton.appendChild(icon);
-
-        // Добавляем кнопку в контейнер
-        container.appendChild(shareButton);
-
-        // Добавляем контейнер в карточку
-        card.appendChild(container);
-    });
-});
-
 $(document).ready(function () {
     // Инициализация всех popup-блоков
     $('.popup-fade').each(function () {
@@ -305,6 +250,61 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }, 500); 
+});
+
+// Находим все элементы с классом `content-card`
+$(document).ready(function () {
+    document.querySelectorAll('.content-card').forEach((card) => {
+
+        const fullWidthContainer = document.createElement('div');
+        fullWidthContainer.className = 'full-width-container';
+
+        const fullWidthBackground = document.createElement('div');
+        fullWidthBackground.className = 'full-width-background';
+
+        const fullWidthContent = document.createElement('div');
+        fullWidthContent.className = 'full-width-content';
+
+        card.parentNode.insertBefore(fullWidthContainer, card); 
+        fullWidthContent.appendChild(card); 
+        fullWidthBackground.appendChild(fullWidthContent); // 
+        fullWidthContainer.appendChild(fullWidthBackground);
+        // Создаем контейнер внутри карточки
+        const container = document.createElement('div');
+        container.className = 'content-card__container';
+
+        // Перемещаем текущие дочерние элементы в контейнер
+        while (card.firstChild) {
+            container.appendChild(card.firstChild);
+        }
+
+    // Создаем ссылку с кнопкой "Поделиться"
+    const shareButton = document.createElement('button');
+    shareButton.className =
+        'button button_link button_white button_size_m button_icon-left social_share';
+    shareButton.href = '/';
+    shareButton.setAttribute('attributes', '{}');
+
+        // Создаем содержимое кнопки
+        const buttonSpan = document.createElement('span');
+        buttonSpan.className = 'button_span';
+        buttonSpan.textContent = 'Поделиться';
+
+        const icon = document.createElement('ion-icon');
+        icon.className = 'icon md hydrated';
+        icon.setAttribute('name', 'link-outline');
+        icon.setAttribute('role', 'img');
+
+        // Собираем структуру кнопки
+        shareButton.appendChild(buttonSpan);
+        shareButton.appendChild(icon);
+
+        // Добавляем кнопку в контейнер
+        container.appendChild(shareButton);
+
+        // Добавляем контейнер в карточку
+        card.appendChild(container);
+    });
 });
 
 function getLength(event) {
