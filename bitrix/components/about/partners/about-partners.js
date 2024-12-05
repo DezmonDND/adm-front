54ae4561c5d31a41b38d9fba951dd211d5d39320
unknown <denedoseikin@mail.ru>
2024-12-05 15:31:18 +0700

    fix: исправление замечаний

---
+++ bitrix/components/about/partners/about-partners.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#about-partners__row').simplemarquee({
+        direction: 'left',
+        space: 24,
+    });
+    $('#about-partners__row-reverse').simplemarquee({
+        direction: 'right',
+        space: 24,
+    });
+});
