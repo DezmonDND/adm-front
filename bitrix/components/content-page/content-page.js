2ec5b76f30fea3c62946a6ada8579c4a3d419bb9
unknown <denedoseikin@mail.ru>
2024-12-10 15:42:10 +0700

    fix: исправить скрипт для подмены иконок

--- bitrix/components/content-page/content-page.js
+++ bitrix/components/content-page/content-page.js
@@ -2,10 +2,12 @@ function replaceIcons() {
     const content = document.querySelector('.content');
 
     if (content) {
-        const icons = document.querySelectorAll('.glyphicon-save');
-
+        const icons = content.querySelectorAll('.glyphicon-save');
         if (icons) {
             icons.forEach((icon) => {
+                const link = icon.closest('a');
+                const container = link.closest('p');
+                container.style.display = 'flex';
                 const newIcon = document.createElement('ion-icon');
                 newIcon.classList.add('icon');
                 newIcon.name = 'download-outline';
