975bfd72854bf9b47c135fa6a514d5266422afb8
Артём Оганесян <ar.oganesyan@atlassys.tech>
2024-12-11 01:26:32 +0300

    Сборка

---
+++ bitrix/components/init-projects/balloon/template.html.php
@@ -0,0 +1,19 @@
+
+<div class="balloon"> 
+  <div class="balloon-title">
+    <div class="balloon-name"> <span>Инициативный проект очень длинное название инициативный проект </span></div>
+    <div class="balloon-btns"> 
+      <ion-icon name="link-outline" onclick=""></ion-icon>
+      <ion-icon name="close" onclick=""></ion-icon>
+    </div>
+  </div>
+  <div class="balloon-img"> <img src="/resources/image_1.jpg"/></div>
+  <div class="balloon-info">
+    <div class="balloon-info-item"><span>Дата внесения</span><span>23 сентября 2024 года</span></div>
+    <div class="balloon-info-item"><span>Срок реализации</span><span>2 года</span></div>
+    <div class="balloon-info-item"><span>Территория реализации</span><span>Ханты-Мансийский автономный округ – Югра</span></div>
+    <div class="balloon-info-item"><span>Инициатор</span><span>Дума города Сургута</span></div>
+    <div class="balloon-info-item"><span>Общая стоимость</span><span>2 978 321,00</span></div>
+    <div class="balloon-info-item"><span>Статус проекта</span><span>Победитель конкурса</span></div>
+  </div>
+</div>
