9756f6670693365767c1c03bcc712d588cd23ea8
unknown <denedoseikin@mail.ru>
2024-12-12 19:41:48 +0700

    fix: исправить табиндекс

--- src/blocks/components/tabs/tabs.js
+++ src/blocks/components/tabs/tabs.js
@@ -15,51 +15,54 @@ function removeTabindex() {
     });
 
     const headerLinks = document.querySelectorAll('.header__link');
-
-    if (headerLinks) {
-        headerLinks.forEach((link) => {
-            link.setAttribute('tabindex', '0');
-        });
-    }
-
     const headerButtonsContainer = document.querySelector('.header__buttons');
+    const headerServices = document.querySelector('.header-services__container');
+    const tabs = document.querySelector('.tabs__container');
+    const filtersPopup = document.querySelector('.filters-popup');
+    const arrows = document.querySelectorAll('.slider__link');
+    const navLinks = document.querySelectorAll('.navigation__link');
+    const addFileButtons = document.querySelectorAll('.add-file-button');
+    const toggleContainers = document.querySelectorAll('.toggle-container');
+    const checkboxes = document.querySelectorAll('.сheckbox__label');
 
     if (headerButtonsContainer) {
         const buttons = headerButtonsContainer.querySelectorAll('.button');
-
-        buttons.forEach((button) => {
-            button.setAttribute('tabindex', '0');
-        });
+        addAttribute(buttons);
     }
-
-    const headerServices = document.querySelector('.header-services__container');
-
     if (headerServices) {
         const buttons = headerServices.querySelectorAll('a');
-
-        buttons.forEach((button) => {
-            button.setAttribute('tabindex', '0');
-        });
+        addAttribute(buttons);
     }
-
-    const tabs = document.querySelector('.tabs__container');
     if (tabs) {
         const buttons = tabs.querySelectorAll('a');
-        buttons.forEach((button) => {
-            button.setAttribute('tabindex', '0');
-        });
+        addAttribute(buttons);
     }
-
-    const filtersPopup = document.querySelector('.filters-popup');
-
     if (filtersPopup) {
         if (window.innerWidth > 992) {
             filtersPopup.style.display = 'none';
         }
     }
+    if (addFileButtons) {
+        addFileButtons.forEach((button) => {
+            const label = button.querySelector('label');
+            label.setAttribute('tabindex', '0');
+        });
+    }
+
+    addAttribute(headerLinks);
+    addAttribute(arrows);
+    addAttribute(navLinks);
+    addAttribute(toggleContainers);
+    addAttribute(checkboxes);
 }
 
-removeTabindex();
+function addAttribute(items) {
+    if (items) {
+        items.forEach((item) => {
+            item.setAttribute('tabindex', '0');
+        });
+    }
+}
 
 function addAriaForIcons() {
     const icons = document.querySelectorAll('ion-icon');
@@ -81,4 +84,5 @@ function addAriaForIcons() {
     }
 }
 
+removeTabindex();
 addAriaForIcons();
