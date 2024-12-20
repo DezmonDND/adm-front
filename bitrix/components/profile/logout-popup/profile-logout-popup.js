a062d09b42fb0075d3afecbf76b7c8c9750d8a03
unknown <denedoseikin@mail.ru>
2024-12-20 15:58:52 +0700

    feat: сделать попап для выхода из ЛК

---
+++ bitrix/components/profile/logout-popup/profile-logout-popup.js
@@ -0,0 +1,28 @@
+function closeLogoutPopup() {
+    const popup = document.querySelector('.profile-logout-popup');
+
+    if (popup) {
+        popup.classList.remove('profile-logout-popup_open');
+        document.querySelector('body').classList.remove('overlay_mobile-menu');
+    }
+}
+
+function showLogout() {
+    const popup = document.querySelector('.profile-logout-popup');
+
+    popup.classList.add('profile-logout-popup_open');
+    document.querySelector('body').classList.add('overlay_mobile-menu');
+}
+
+document.addEventListener('click', (e) => {
+    const openedPopup = document.querySelector('.profile-logout-popup_open');
+
+    if (
+        openedPopup &&
+        !e.target.closest('.profile-logout-popup__content') &&
+        !e.target.classList.contains('button_exit')
+    ) {
+        openedPopup.classList.remove('profile-logout-popup_open');
+        document.querySelector('body').classList.remove('overlay_mobile-menu');
+    }
+});
